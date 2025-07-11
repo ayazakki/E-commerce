e267a03de9ba12e4ae380b2faa28b50385b34297
ayazakki <ayai42607@gmail.com>
2025-07-12 02:14:31 +0300

    update path to flowbite js

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import './index.css'
 import App from './App.jsx'
 import "@fortawesome/fontawesome-free/css/all.min.css"
-import "../node_modules/flowbite";
+import 'flowbite/dist/flowbite.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
